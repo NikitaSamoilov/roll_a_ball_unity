581c08c033c61ec60bcbb855874b1a08bd60529e
NikitaSamoilov <nswork1987@gmail.com>
2015-05-31 14:18:28 +0300

    First commit

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	public float rotateX = 15.0f;
+	public float rotateY = 30.0f;
+	public float rotateZ = 45.0f;
+
+	void Update ()
+	{
+		transform.Rotate (new Vector3 (rotateX, rotateY, rotateZ) * Time.deltaTime);
+	}
+}
